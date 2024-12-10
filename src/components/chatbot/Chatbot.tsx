301ba7052f869e4436cebfb3e3559171a95d236c
enguyen111 <neric77@gmail.com>
2024-12-09 23:40:57 -0500

    feat: by default the send button is invisible but toggles when users starts entering

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -3,13 +3,24 @@ import { IoMdSend } from "react-icons/io";
 import './index.css';
 import { CiChat1 } from "react-icons/ci";
 import { IoCloseSharp } from "react-icons/io5";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 function Chatbot() {
     const [chatOpen, setChatOpen] = useState(false);
+    const [outgoingMsg, setOutgoingMsg] = useState("");
 
-    const handleChatToggle= () => {
+    const handleChatToggle = () => {
         setChatOpen(!chatOpen);
+    }
+
+    const handleOutgoingMsg = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setOutgoingMsg(e.target.value);
+        const sendBtnVisibility = document.getElementById('send-btn');
+        if (e.target.value.trim().length !== 0 && sendBtnVisibility !== null) {
+            sendBtnVisibility.style.visibility = 'visible';
+        }else if (sendBtnVisibility !== null) {
+            sendBtnVisibility.style.visibility = 'hidden';
+        }
     }
     return (
         <>
@@ -28,7 +39,10 @@ function Chatbot() {
                         </li>
                     </ul>
                     <div className="chat-input">
-                        <textarea placeholder="Enter your message here..." required ></textarea>
+                        <textarea placeholder="Enter your message here..." required 
+                        value={outgoingMsg} onChange={(e) => handleOutgoingMsg(e)}>
+
+                        </textarea>
                         <span id="send-btn"><IoMdSend /></span>
                     </div>
                 </div>
